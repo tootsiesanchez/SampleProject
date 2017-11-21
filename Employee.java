208a847ebf53e207428503d903ffb6ea9c9f68d1
Tootsie Sanchez <anthonette@pointwest.com.ph>
2017-11-21 19:43:04 +0800

    Added Employee bean

---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+	private int employeeId;
+	
+	public Employee(){
+	
+	}
+
+	public void setEmployeeId(int employeeId){
+		this.employeeId = employeeId;
+	}
+	
+	public vint getEmployeeId{
+	return employeeId;
+	}
+}
